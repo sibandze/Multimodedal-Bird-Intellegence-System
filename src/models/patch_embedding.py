db362cd0306a5e728a411197bf362fea71563821
N Sibandze <56678793+sibandze@users.noreply.github.com>
2026-09-08 20:59:53 +0200

    Refactor input comments and clean up code formatting

--- src/models/patch_embedding.py
+++ src/models/patch_embedding.py
@@ -1,32 +1,32 @@
 # src/models/patch_embedding.py
 """
-    Input
-    -----
-    (B, 128, 600)
-    
-    ↓
-    
-    Patch size = 25 frames
-    
-    ↓
-    
-    24 patches
-    
-    ↓
-    
-    Flatten
-    
-    ↓
-    
-    (B, 24, 3200)
-    
-    ↓
-    
-    Linear Projection
-    
-    ↓
-    
-    (B, 24, 256)
+Input
+-----
+(B, 128, 600)
+
+↓
+
+Patch size = 25 frames
+
+↓
+
+24 patches
+
+↓
+
+Flatten
+
+↓
+
+(B, 24, 3200)
+
+↓
+
+Linear Projection
+
+↓
+
+(B, 24, 256)
 """
 
 import torch
@@ -35,9 +35,9 @@ import torch.nn as nn
 
 class SpectrogramPatchEmbedding(nn.Module):
     def __init__(
-        self, 
-        n_mels: int, 
-        patch_size: int, 
+        self,
+        n_mels: int,
+        patch_size: int,
         embed_dim: int,
     ):  # (128, 25, 256)
         super().__init__()
@@ -48,19 +48,14 @@ class SpectrogramPatchEmbedding(nn.Module):
         # each patch is flattened then projected
         self.projection = nn.Linear(n_mels * patch_size, embed_dim)
 
-    def forward(
-        self,
-        x: torch.Tensor
-    ) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         x: (B, n_mels, time)
         returns: (B, num_patches, embed_dim)
         """
         B, M, T = x.shape
-        assert M == self.n_mels, (
-            f"Expected {self.n_mels} Mel bins, got {M}."
-        )
-        
+        assert M == self.n_mels, f"Expected {self.n_mels} Mel bins, got {M}."
+
         # ensure divisible
         T_trim = (T // self.patch_size) * self.patch_size
         x = x[:, :, :T_trim]
@@ -74,7 +69,7 @@ class SpectrogramPatchEmbedding(nn.Module):
         x = x.permute(0, 2, 1, 3)
         # (B, num_patches, M, patch_size)
         x = x.contiguous()
-        
+
         # flatten each patch
         x = x.reshape(B, -1, M * self.patch_size)
         # (B, num_patches, patch_dim)
